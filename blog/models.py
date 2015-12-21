3ad631b26b804f9763666070f6da3cb6c7725591
annkpx <annkpx@gmail.com>
2015-12-21 14:44:09 +0300

    причёсывание внешнего вида

--- blog/models.py
+++ blog/models.py
@@ -28,7 +28,7 @@ class Article(models.Model):
     name = models.CharField(max_length=128, verbose_name=u'название')
     date = models.DateField(verbose_name=u'дата')
     text = RichTextField(verbose_name=u'текст')
-    desc = models.CharField(max_length=512, verbose_name=u'сокращенный текст')
+    desc = models.TextField(max_length=512, verbose_name=u'сокращенный текст')
     tags = TaggableManager(through=ArticleTaggedItem, blank=True)
     slug = models.SlugField(verbose_name=u'slug', unique=True, blank=True, help_text=u'Заполнять не нужно')
     image = models.ImageField(upload_to='uploads/items', max_length=256, blank=True, verbose_name=u'изображение')
@@ -47,4 +47,3 @@ class Article(models.Model):
         if not self.slug:
             self.slug=pytils.translit.slugify(self.name)
         super(Article, self).save(*args, **kwargs)
-        
